9f75911679090fa75b7f66d477ad2cfc9c18265c
VitaminB16 <artemiy.nosov@gmail.com>
2024-08-30 20:05:09 +0100

    chore: Move logic into src

--- ba_points/src/src.py
+++ ba_points/src/src.py
@@ -1,14 +1,27 @@
+import pandas as pd
 from ba_points.src.api_scraper import get_destinations_prices, get_tier_points
 
 
 def get_prices_tier_points(number_of_nights=1):
-    destination_prices = get_destinations_prices(number_of_nights=number_of_nights)
+    if not isinstance(number_of_nights, (list, range, tuple)):
+        number_of_nights = [number_of_nights]
+
+    dfs = []
+    for number_of_nights in range(1, 8):
+        _df = get_destinations_prices(number_of_nights=number_of_nights)
+        _df["number_of_nights"] = number_of_nights
+        dfs.append(_df)
+    dfs = pd.concat(dfs)
+    destination_prices = dfs.reset_index(drop=True)
+
     tier_points = get_tier_points()
+
     df = destination_prices.merge(
         tier_points, on=["arr_city_name", "cabin"], how="left"
     )
     df = df.query("tier_points.notnull()")
     points_per_pound = 2 * df["tier_points"] / df["lowest_price"]
     df = df.assign(tier_points_per_pound=round(points_per_pound, 2))
+    df = df.sort_values("tier_points_per_pound", ascending=False)
     df.to_csv("data/prices_tier_points.csv", index=False)
     return df
--- main.py
+++ main.py
@@ -8,14 +8,6 @@ def main(number_of_nights=1):
 
 
 if __name__ == "__main__":
-    dfs = []
-    for number_of_nights in range(1, 8):
-        df = main(number_of_nights=number_of_nights)
-        df["number_of_nights"] = number_of_nights
-        dfs.append(df)
-    df = pd.concat(dfs)
-    df = df.sort_values("tier_points_per_pound", ascending=False)
-    df = df.reset_index(drop=True)
-    print(df.head(20))
+    df = main(number_of_nights=range(1, 8))
     df.to_csv("data/prices_tier_points_sorted.csv", index=False)
     breakpoint()
