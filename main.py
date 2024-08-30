259613c63a79e3a63e30f3e5d145077d6b6034b4
VitaminB16 <artemiy.nosov@gmail.com>
2024-08-31 00:13:25 +0100

    feat: All number of nights up to 7

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from ba_points.src.src import get_prices_tier_points
 
 
@@ -7,9 +8,14 @@ def main(number_of_nights=1):
 
 
 if __name__ == "__main__":
-    df = main(number_of_nights=1)
+    dfs = []
+    for number_of_nights in range(1, 8):
+        df = main(number_of_nights=number_of_nights)
+        df["number_of_nights"] = number_of_nights
+        dfs.append(df)
+    df = pd.concat(dfs)
     df = df.sort_values("tier_points_per_pound", ascending=False)
     df = df.reset_index(drop=True)
-    print(df.head(50))
+    print(df.head(20))
     df.to_csv("data/prices_tier_points_sorted.csv", index=False)
     breakpoint()
